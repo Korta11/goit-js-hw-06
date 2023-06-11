704f1449bcd5049293988fc07de28a4e658914db
Korta11 <130395545+Korta11@users.noreply.github.com>
2023-06-11 11:33:54 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,15 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  ingredientsList.append(item);
+});
