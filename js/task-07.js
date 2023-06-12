696a778feb05d46725107b3f6c985c3f99f3ebbe
Korta11 <130395545+Korta11@users.noreply.github.com>
2023-06-12 13:19:25 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("input#font-size-control");
 const span = document.querySelector("span#text");
 
-input.addEventListener("change", (size) => {
+input.addEventListener("input", (size) => {
   span.style.fontSize = `${size.target.value}px`;
 });
