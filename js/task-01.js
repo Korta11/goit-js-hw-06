33df7fe686442e75add1e448219f62d05e21acfc
Korta11 <130395545+Korta11@users.noreply.github.com>
2023-06-12 13:19:18 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,7 @@
-const ulList = document.querySelector("#categories");
-const items = ulList.querySelectorAll(".item");
-console.log(`Number of categories: ${items.length}`);
+const ulList = document.querySelectorAll(".item");
+console.log(`Number of categories: ${ulList.length}`);
 
-items.forEach((item) => {
-  const title = item.querySelector("h2").textContent;
-  const itemCount = item.querySelectorAll("li").length;
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${itemCount}`);
+ulList.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
